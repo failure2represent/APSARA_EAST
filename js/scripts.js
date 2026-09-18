64c86072b68282f3d9c99d6ff435ad75f8285b3f
failure22 <markvalonski@gmail.com>
2026-09-18 04:34:15 +0200

    all_media_q_w/_bugs+++new_about_pic

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,19 @@
 document.querySelectorAll('.accordion__item').forEach((item) => {
-	const title = item.querySelector('.accordion__title');
 	const content = item.querySelector('.accordion__content');
-	const text = content.querySelector('.accordion__text');
 
-	title.addEventListener('click', () => {
+	item.addEventListener('click', (e) => {
+		if (e.target.closest('.accordion__content')) return;
+
 		const isOpen = item.classList.contains('accordion__item--active');
 
 		document.querySelectorAll('.accordion__item').forEach((other) => {
 			other.classList.remove('accordion__item--active');
-			other.querySelector('.accordion__title').classList.remove('accordion__title--active');
 			other.querySelector('.accordion__content').style.height = '0px';
 		});
 
 		if (!isOpen) {
 			item.classList.add('accordion__item--active');
-			title.classList.add('accordion__title--active');
-			content.style.height = text.scrollHeight + 'px';
+			content.style.height = content.scrollHeight + 'px';
 		}
 	});
 });
@@ -37,3 +35,17 @@ function closeMenu() {
 	nav.classList.remove('active');
 	overlay.classList.remove('active');
 }
+
+//smooth logo
+const logo = document.querySelector('.header__logo');
+
+logo?.addEventListener('click', function (e) {
+	const isHomePage =
+		window.location.pathname.endsWith('/index.html') ||
+		window.location.pathname.endsWith('/');
+
+	if (isHomePage) {
+		e.preventDefault();
+		window.scrollTo({ top: 0, behavior: 'smooth' });
+	}
+});
